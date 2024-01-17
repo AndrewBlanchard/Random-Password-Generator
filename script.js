73d07c16328c31bc4ceab6df463688704105999b
unknown <blanchard0andrew@gmail.com>
2024-01-17 16:47:43 -0600

    First commit

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ var generateBtn = document.querySelector("#generate");
 // Generate a random password 
 function generatePassword() {
   let password = '';
-  let charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?';
+  let charset = '';
 
-// Password lenght between 8 - 128 charcaters in lenght
-// This function helps us generate a random number between two numbers. 
-//function random(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
-let passwordLenght = Math.floor(Math.random() * (128 - 8 + 1)) + 8;
+// Uppercase Prompt
+if (confirm("Would you like to include uppercase letters?")) {
+  charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+}
+// Lowerercase Prompt
+if (confirm("Would you like to include lowercase letters?")) {
+  charset += 'abcdefghijklmnopqrstuvwxyz';
+}
+// Numbers Prompt
+if (confirm("Would you like to include numbers?")) {
+  charset += '0123456789';
+}
+// Uppercase Prompt
+if (confirm("Would you like to include special charcters?")) {
+  charset += '!@#$%^&*()_-+=?<>';
+}
+
+// Password length between 8 - 128 characters in length
+let passwordLength = prompt("How long would you like your random password? (Type a number bewteen 8 and 128):");
+passwordLength = parseInt(passwordLength);
+
+// Validate password length
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  alert("Please enter a valid password length between 8 and 128.");
+  return "";
+}
+
 //Loop
-  for (let i = 0; i < passwordLenght; i++) {
-    let randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
-  }
+for (let i = 0; i < passwordLength; i++) {
+  let randomIndex = Math.floor(Math.random() * charset.length);
+  password += charset.charAt(randomIndex);
+}
 
   return password;
 }
